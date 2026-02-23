fac25e62104149871d49ef50f54a57ef627cd716
Buddha7771 <hwlee7771@gmail.com>
2026-02-24 00:45:00 +0900

    replace pyright with ty

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,11 @@
 return {
   "nvim-telescope/telescope.nvim",
-  tag = "0.1.8",
+  opts = {
+    defaults = {
+      preview = {
+        -- 미리보기 창에서 Treesitter 끄기 (에러 방지용)
+        treesitter = false,
+      },
+    },
+  },
 }
